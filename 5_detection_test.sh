508eae8cb06024887601530e31aa16335dd8d1d1
clearmedium <cl3arm3dium@gmail.com>
2019-05-24 23:09:23 -0400

    Bad for loop variable

--- 5_detection_test.sh
+++ 5_detection_test.sh
@@ -1,10 +1,20 @@
 #!/bin/sh
 
-counter="52"
-remainder=$(( counter % 5 ))
-echo "Counter is $counter"
-if [ "$remainder" -eq 0 ]; then
-    echo 'its a multiple of 5'
-else
-    echo 'its not a multiple of 5'
-fi
+
+for ((n=1;n<=100;n++)); 
+do
+   calc $n/5
+done
+
+
+#https://www.cyberciti.biz/faq/unix-for-loop-1-to-10/
+
+
+# counter="55"
+# remainder=$(( counter % 5 ))
+# #echo "Counter is $counter"
+# if [ "$remainder" -eq 0 ]; then
+#     echo 'fizz'
+# else
+#     echo $counter
+# fi
